ca57ad405ec352064aa388afd1db783ea9e8db97
lkorth <luke@lukekorth.com>
2017-01-06 15:01:21 -0500

    Fix back button during browser switch on Samsung devices

--- browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchFragment.java
+++ browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchFragment.java
@@ -92,7 +92,8 @@ public abstract class BrowserSwitchFragment extends Fragment {
             intent.putExtras(extras);
             intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK |
                     Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS |
-                    Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK);
         }
 
         browserSwitch(intent);
