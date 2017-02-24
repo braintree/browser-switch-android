285537ab9e8c096e2ba908d3c0335bf994cca2f7
lkorth <luke@lukekorth.com>
2017-02-23 20:41:19 -0800

    Add simple release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Make sure to update the version in build.gradle and the README."
+echo "Press enter when you are ready to release."
+read
+
+./gradlew --info clean lint test
+./gradlew :browser-switch:uploadArchives :browser-switch:closeAndPromoteRepository
+
+echo "Release complete. Be sure to commit, tag and push your changes."
+echo "After the tag has been pushed, update the releases tab on GitHub with the changes for this release."
+echo "\n"
+read
