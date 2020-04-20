ffddf1da3a5c40122fe42a02bed53e200ba7adb6
Carmen Alvarez <c@rmen.ca>
2020-04-20 09:36:46 +0200

    DemoActivity: improve the fix to avoid adding multiple DemoFragments
    Check if the fragment is already added before adding it. To be able to find the fragment later, we need to add it with a tag.

--- demo/src/main/java/com/braintreepayments/browserswitch/demo/DemoActivity.java
+++ demo/src/main/java/com/braintreepayments/browserswitch/demo/DemoActivity.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class DemoActivity extends FragmentActivity {
+
+    private static final String FRAGMENT_TAG = DemoFragment.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, new DemoFragment())
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.findFragmentByTag(FRAGMENT_TAG) == null) {
+            fm.beginTransaction()
+                    .add(android.R.id.content, new DemoFragment(), FRAGMENT_TAG)
                     .commit();
         }
     }
