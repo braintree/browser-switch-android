a726fb262e4d0669f74f40000f55efa22cb6c831
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-05-28 09:43:45 -0500

    Spike chrome custom tabs with callback fn.

---
+++ browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserCustomTabsCallback.java
@@ -0,0 +1,37 @@
+package com.braintreepayments.browserswitch;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsCallback;
+
+public class BrowserCustomTabsCallback extends CustomTabsCallback {
+
+    private static final String TAG = "BrowserCustomTabs";
+
+    @Override
+    public void onNavigationEvent(int navigationEvent, @Nullable Bundle extras) {
+        super.onNavigationEvent(navigationEvent, extras);
+        Log.d(TAG, getNavigationEventName(navigationEvent));
+    }
+
+    private static String getNavigationEventName(int navigationEvent) {
+        switch (navigationEvent) {
+            case CustomTabsCallback.NAVIGATION_STARTED:
+                return "NAVIGATION_STARTED";
+            case CustomTabsCallback.NAVIGATION_FINISHED:
+                return "NAVIGATION_FINISHED";
+            case CustomTabsCallback.NAVIGATION_FAILED:
+                return "NAVIGATION_FAILED";
+            case CustomTabsCallback.NAVIGATION_ABORTED:
+                return "NAVIGATION_ABORTED";
+            case CustomTabsCallback.TAB_SHOWN:
+                return "TAB_SHOWN";
+            case CustomTabsCallback.TAB_HIDDEN:
+                return "TAB_HIDDEN";
+            default:
+                return "UNKNOWN";
+        }
+    }
+}
---
+++ browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserCustomTabsClient.java
@@ -0,0 +1,47 @@
+package com.braintreepayments.browserswitch;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.browser.customtabs.CustomTabsClient;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.browser.customtabs.CustomTabsServiceConnection;
+import androidx.browser.customtabs.CustomTabsSession;
+
+// Ref: https://developer.chrome.com/multidevice/android/customtabs
+public class BrowserCustomTabsClient extends CustomTabsServiceConnection {
+
+    private static final String TAG = "BrowserCustomTabs";
+    private static final String CHROME_PACKAGE_NAME = "com.android.chrome";
+
+    private Uri targetUri;
+    private Context context;
+
+    private CustomTabsClient customTabsClient;
+    private CustomTabsSession customTabsSession;
+
+    public boolean startSession(Context context, Uri uri) {
+        this.context = context;
+        this.targetUri = uri;
+        return CustomTabsClient.bindCustomTabsService(context, CHROME_PACKAGE_NAME, this);
+    }
+
+    @Override
+    public void onCustomTabsServiceConnected(@NonNull ComponentName name, @NonNull CustomTabsClient client) {
+        customTabsClient = client;
+        Log.d(TAG, "Custom Tabs Service Connected");
+        customTabsSession = customTabsClient.newSession(new BrowserCustomTabsCallback());
+
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder(customTabsSession)
+                .build();
+        customTabsIntent.launchUrl(context, targetUri);
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        Log.d(TAG, "Custom Tabs Service Disconnected");
+    }
+}
