75d7e9c13e92f723a916f93f43ebf2a6747f430a
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-06-12 10:12:40 -0500

    Restrict BrowserSwitchStatus int in BrowserSwitchResult.

--- browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchResult.java
+++ browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchResult.java
@@ -1,23 +1,33 @@
 package com.braintreepayments.browserswitch;
 
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class BrowserSwitchResult {
 
     final private int status;
     final private String errorMessage;
 
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({ STATUS_OK, STATUS_CANCELED, STATUS_ERROR })
+    public @interface BrowserSwitchStatus {}
+
     public static final int STATUS_OK = 1;
     public static final int STATUS_CANCELED = 2;
     public static final int STATUS_ERROR = 3;
 
-    BrowserSwitchResult(int status) {
+    BrowserSwitchResult(@BrowserSwitchStatus int status) {
         this(status, null);
     }
 
-    BrowserSwitchResult(int status, String errorMessage) {
+    BrowserSwitchResult(@BrowserSwitchStatus int status, String errorMessage) {
         this.status = status;
         this.errorMessage = errorMessage;
     }
 
+    @BrowserSwitchStatus
     public int getStatus() {
         return status;
     }
