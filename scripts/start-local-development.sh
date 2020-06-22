abd9bb66f1ce6a09fa0b420a3fe555039f0fa94c
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-06-22 15:41:21 -0500

    Cleanup gradle file and local development script.

--- scripts/start-local-development.sh
+++ scripts/start-local-development.sh
@@ -3,10 +3,6 @@
 # exit with failure when an error occurs
 set -e
 
-# Allow script to be run from parent directory, and also from the base directory of the project
-# Ref: https://stackoverflow.com/a/246128
-SCRIPT_PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BASE_DIR=$(realpath "${SCRIPT_PARENT_DIR}/..")
-
 # run gradle in continuous build mode to upload archives to local maven when changes occur
-${BASE_DIR}/gradlew -PbuildType=DEVELOPMENT -t assembleDebug uploadArchives 
+./gradlew -t assembleDebug uploadArchives 
+
