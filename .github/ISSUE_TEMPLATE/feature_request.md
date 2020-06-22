29c03052464407f89ee2f756a41601447e14e2e6
sshropshire <58225613+sshropshire@users.noreply.github.com>
2020-06-22 16:48:42 -0500

    Create Rakefile (#25)
    * Add tests for chrome custom tabs to check Android versions that support the feature and other Android versions that do not.
    
    * Cleanup gradle file and local development script.
    
    * Replace release.sh script with Rakefile.
    
    * Add missing lint task in Rakefile.
    
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>
    
    * Bump major version in CHANGELOG.
    
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>
    
    * Update issue and PR templates.
    
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>
    
    * Revert latest version to 'unreleased'.
    
    * Remove translation message from issue template.
    
    Co-authored-by: Samantha Cannillo <samantha.cannillo@getbraintree.com>

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,11 @@
+---
+name: Feature request
+about: Suggest an idea for our SDK
+---
+
+**Is your feature request related to a problem? Please describe.**
+A clear and concise description of what the problem is.
+
+**Describe the solution you'd like**
+Description of what you want to happen. Follow the [user story](https://en.wikipedia.org/wiki/User_story) format to clearly describe the use case.
+
--- browser-switch/src/androidTest/java/com/braintreepayments/browserswitch/ChromeCustomTabsTest.java
+++ browser-switch/src/androidTest/java/com/braintreepayments/browserswitch/ChromeCustomTabsTest.java
@@ -1,11 +1,14 @@
 package com.braintreepayments.browserswitch;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SdkSuppress;
 
+import com.braintreepayments.browserswitch.test.BuildConfig;
 import com.braintreepayments.browserswitch.test.TestBrowserSwitchFragment;
 
 import org.junit.Before;
@@ -13,7 +16,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
 @RunWith(AndroidJUnit4.class)
@@ -27,7 +34,8 @@ public class ChromeCustomTabsTest {
     }
 
     @Test
-    public void isAvailable_returnsFalseIfChromeCustomTabsAreNotSupported() {
+    @SdkSuppress(maxSdkVersion = 23)
+    public void isAvailable_whenCustomTabsAreNotSupported_returnsFalse() {
         scenario.onFragment(fragment -> {
             FragmentActivity activity = fragment.getActivity();
             assertFalse(ChromeCustomTabs.isAvailable(activity.getApplication()));
@@ -35,12 +43,35 @@ public class ChromeCustomTabsTest {
     }
 
     @Test
-    public void addChromeCustomTabsExtras_doesNotModifyTheIntentIfChromeCustomTabsAreNotSupported() {
+    @SdkSuppress(minSdkVersion = 24)
+    public void isAvailable_whenCustomTabsAreSupported_returnsTrue() {
+        scenario.onFragment(fragment -> {
+            FragmentActivity activity = fragment.getActivity();
+            assertTrue(ChromeCustomTabs.isAvailable(activity.getApplication()));
+        });
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = 23)
+    public void addChromeCustomTabsExtras_whenCustomTabsAreNotSupported_doesNotModifyTheIntent() {
         Intent intent = mock(Intent.class);
         scenario.onFragment(fragment -> {
             FragmentActivity activity = fragment.getActivity();
             ChromeCustomTabs.addChromeCustomTabsExtras(activity.getApplication(), intent);
             verifyZeroInteractions(intent);
+        });
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = 24)
+    public void addChromeCustomTabsExtras_whenCustomTabsAreSupported_modifiesTheIntent() {
+        Intent intent = mock(Intent.class);
+        scenario.onFragment(fragment -> {
+            FragmentActivity activity = fragment.getActivity();
+            ChromeCustomTabs.addChromeCustomTabsExtras(activity.getApplication(), intent);
+
+            verify(intent).putExtras(any(Bundle.class));
+            verify(intent).addFlags(anyInt());
         });
     }
 }
