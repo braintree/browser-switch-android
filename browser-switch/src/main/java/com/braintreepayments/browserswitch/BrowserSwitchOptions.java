5db00823ec7d9df6a4fce5ef0a1da64004742dc2
Sarah Koop <skoop@paypal.com>
2020-07-29 14:08:40 -0500

    Add java docs to BrowserSwitchOptions class.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchOptions.java
+++ browser-switch/src/main/java/com/braintreepayments/browserswitch/BrowserSwitchOptions.java
@@ -19,38 +19,70 @@ public class BrowserSwitchOptions {
     private int requestCode;
     private Uri url;
 
+    /**
+     * Set browser switch intent
+     * @param intent The target intent to use for browser switch. Required unless url specified
+     * @return {@link BrowserSwitchOptions} returns reference to instance to allow setter invocations to be chained
+     */
     public BrowserSwitchOptions intent(Intent intent) {
         this.intent = intent;
         return this;
     }
 
+    /**
+     * Set browser switch metadata
+     * @param metadata JSONObject containing metadata to associate with the browser switch request
+     * @return {@link BrowserSwitchOptions} returns reference to instance to allow setter invocations to be chained
+     */
     public BrowserSwitchOptions metadata(JSONObject metadata) {
         this.metadata = metadata;
         return this;
     }
 
+    /**
+     * Set browser switch request code
+     * @param requestCode Request code int to associate with the browser switch request
+     * @return {@link BrowserSwitchOptions} returns reference to instance to allow setter invocations to be chained
+     */
     public BrowserSwitchOptions requestCode(int requestCode) {
         this.requestCode = requestCode;
         return this;
     }
 
+    /**
+     * Set browser switch url
+     * @param url The target url to use for browser switch. Required unless intent specified
+     * @return {@link BrowserSwitchOptions} returns reference to instance to allow setter invocations to be chained
+     */
     public BrowserSwitchOptions url(Uri url) {
         this.url = url;
         return this;
     }
 
+    /**
+     * @return The target intent used for browser switch
+     */
     public Intent getIntent() {
         return intent;
     }
 
+    /**
+     * @return The metadata associated with the browser switch request
+     */
     public JSONObject getMetadata() {
         return metadata;
     }
 
+    /**
+     * @return The request code associated with the browser switch request
+     */
     public int getRequestCode() {
         return requestCode;
     }
 
+    /**
+     * @return The target url used for browser switch
+     */
     public Uri getUrl() {
         return url;
     }
