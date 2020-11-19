85abf52a53ff1e3c72575f7ea3e8fb3ac4f9ce86
Sarah Koop <skoop@paypal.com>
2020-11-19 15:36:27 -0600

    Fix demo app compliation error.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- demo/src/main/java/com/braintreepayments/browserswitch/demo/DemoFragment.java
+++ demo/src/main/java/com/braintreepayments/browserswitch/demo/DemoFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 
 import com.braintreepayments.browserswitch.BrowserSwitchClient;
+import com.braintreepayments.browserswitch.BrowserSwitchException;
 import com.braintreepayments.browserswitch.BrowserSwitchListener;
 import com.braintreepayments.browserswitch.BrowserSwitchOptions;
 import com.braintreepayments.browserswitch.BrowserSwitchResult;
@@ -89,7 +90,11 @@ public class DemoFragment extends Fragment implements View.OnClickListener, Brow
         BrowserSwitchOptions browserSwitchOptions = new BrowserSwitchOptions()
                 .requestCode(1)
                 .url(url);
-        browserSwitchClient.start(browserSwitchOptions, getActivity(), this);
+        try {
+            browserSwitchClient.start(browserSwitchOptions, getActivity());
+        } catch (BrowserSwitchException e) {
+            e.printStackTrace();
+        }
     }
 
     private void startBrowserSwitchWithMetadata(JSONObject metadata) {
@@ -98,7 +103,11 @@ public class DemoFragment extends Fragment implements View.OnClickListener, Brow
                 .metadata(metadata)
                 .requestCode(1)
                 .url(url);
-        browserSwitchClient.start(browserSwitchOptions, getActivity(), this);
+        try {
+            browserSwitchClient.start(browserSwitchOptions, getActivity());
+        } catch (BrowserSwitchException e) {
+            e.printStackTrace();
+        }
     }
 
     private Uri buildBrowserSwitchUrl() {
