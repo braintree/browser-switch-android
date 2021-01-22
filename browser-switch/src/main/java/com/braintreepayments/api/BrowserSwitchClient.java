f8f9ad5287f9c7c1ba74911a6f8e14746210f451
Sarah Koop <skoop@paypal.com>
2021-01-22 12:05:35 -0600

    Cleanup linter error.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- browser-switch/src/main/java/com/braintreepayments/api/BrowserSwitchClient.java
+++ browser-switch/src/main/java/com/braintreepayments/api/BrowserSwitchClient.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.FragmentActivity;
 
