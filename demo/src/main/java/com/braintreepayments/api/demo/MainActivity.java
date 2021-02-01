82038fe51d44f966e704a5610306a61625ac4b58
Sarah Koop <skoop@paypal.com>
2021-02-01 12:01:29 -0600

    Add main activity with launch mode buttons.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

---
+++ demo/src/main/java/com/braintreepayments/api/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.braintreepayments.api.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
