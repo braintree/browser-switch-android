eb2b362958d8aa77845cc0e6d5939c20f1d29759
Steven Shropshire <steven.shropshire@getbraintree.com>
2021-02-03 15:57:31 -0600

    Add docs for BrowserSwitchException
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- browser-switch/src/main/java/com/braintreepayments/api/BrowserSwitchException.java
+++ browser-switch/src/main/java/com/braintreepayments/api/BrowserSwitchException.java
@@ -1,5 +1,8 @@
 package com.braintreepayments.api;
 
+/**
+ * Error class thrown when browser switch returns an error.
+ */
 public class BrowserSwitchException extends Exception {
 
     BrowserSwitchException(String message) {
