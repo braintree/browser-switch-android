305fcaf93c64cf42b834f348ab51094efe2d7e5f
Steven Shropshire <steven.shropshire@getbraintree.com>
2022-09-02 10:22:58 -0500

    Add empty BrowserSwitchResult unit test.
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

---
+++ browser-switch/src/test/java/com/braintreepayments/api/BrowserSwitchResultUnitTest.java
@@ -0,0 +1,9 @@
+package com.braintreepayments.api;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BrowserSwitchResultUnitTest {
+
+}
