91f3167ec3b1908770921976768495d7153b854e
Sarah Koop <skoop@paypal.com>
2024-01-22 10:52:47 -0600

    Remove unecessary unit tests

--- browser-switch/src/test/java/com/braintreepayments/api/BrowserSwitchClientUnitTest.java
+++ browser-switch/src/test/java/com/braintreepayments/api/BrowserSwitchClientUnitTest.java
@@ -1,18 +1,14 @@
 package com.braintreepayments.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,7 +27,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
@@ -42,7 +37,6 @@ public class BrowserSwitchClientUnitTest {
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
 
-    private BrowserSwitchPersistentStore persistentStore;
     private BrowserSwitchInspector browserSwitchInspector;
 
     private ChromeCustomTabsInternalClient customTabsInternalClient;
@@ -57,8 +51,6 @@ public class BrowserSwitchClientUnitTest {
     private ComponentActivity componentActivity;
     @Before
     public void beforeEach() {
-        persistentStore = mock(BrowserSwitchPersistentStore.class);
-
         browserSwitchInspector = mock(BrowserSwitchInspector.class);
         customTabsInternalClient = mock(ChromeCustomTabsInternalClient.class);
 
@@ -198,208 +190,6 @@ public class BrowserSwitchClientUnitTest {
     }
 
     @Test
-    public void deliverResult_whenDeepLinkUrlExistsAndReturnUrlSchemeMatchesAndNoPendingRequest_returnsNull() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-
-        Uri deepLinkUrl = Uri.parse("return-url-scheme://test");
-        Intent deepLinkIntent = new Intent().setData(deepLinkUrl);
-        when(activity.getIntent()).thenReturn(deepLinkIntent);
-
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(null);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNull(result);
-    }
-
-    @Test
-    public void deliverResult_whenDeepLinkUrlExists_AndReturnUrlSchemeDoesNotMatch_AndShouldNotifyCancellation_notifiesResultCANCELED_AndSetsRequestShouldNotifyCancellationToFalse() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-
-        Uri deepLinkUrl = Uri.parse("another-return-url-scheme://test");
-        Intent deepLinkIntent = new Intent().setData(deepLinkUrl);
-        when(activity.getIntent()).thenReturn(deepLinkIntent);
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request = new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", true);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNotNull(result);
-        assertEquals(123, result.getRequestCode());
-        assertSame(browserSwitchDestinationUrl, result.getRequestUrl());
-        assertEquals(result.getStatus(), BrowserSwitchStatus.CANCELED);
-        assertSame(result.getRequestMetadata(), requestMetadata);
-        assertNull(result.getDeepLinkUrl());
-
-        ArgumentCaptor<BrowserSwitchRequest> captor = ArgumentCaptor.forClass(BrowserSwitchRequest.class);
-        verify(persistentStore).putActiveRequest(captor.capture(), same(activity));
-
-        BrowserSwitchRequest updatedRequest = captor.getValue();
-        assertSame(request, updatedRequest);
-        assertFalse(updatedRequest.getShouldNotifyCancellation());
-    }
-
-    @Test
-    public void deliverResult_whenDeepLinkUrlExistsAndReturnUrlSchemeDoesNotMatchAndShouldNotNotifyCancellation_returnsNull() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-
-        Uri deepLinkUrl = Uri.parse("another-return-url-scheme://test");
-        Intent deepLinkIntent = new Intent().setData(deepLinkUrl);
-        when(activity.getIntent()).thenReturn(deepLinkIntent);
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request = new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", false);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNull(result);
-        verify(persistentStore, never()).putActiveRequest(any(BrowserSwitchRequest.class), any(FragmentActivity.class));
-    }
-
-    @Test
-    public void deliverResult_whenDeepLinkUrlDoesNotExistAndShouldNotifyCancellation_notifiesResultCANCELEDAndSetsRequestShouldNotifyCancellationToFalse() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-        when(activity.getIntent()).thenReturn(new Intent());
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request =
-                new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", true);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNotNull(result);
-        assertEquals(123, result.getRequestCode());
-        assertSame(browserSwitchDestinationUrl, result.getRequestUrl());
-        assertEquals(result.getStatus(), BrowserSwitchStatus.CANCELED);
-        assertSame(result.getRequestMetadata(), requestMetadata);
-        assertNull(result.getDeepLinkUrl());
-
-        ArgumentCaptor<BrowserSwitchRequest> captor = ArgumentCaptor.forClass(BrowserSwitchRequest.class);
-        verify(persistentStore).putActiveRequest(captor.capture(), same(activity));
-
-        BrowserSwitchRequest updatedRequest = captor.getValue();
-        assertSame(request, updatedRequest);
-        assertFalse(updatedRequest.getShouldNotifyCancellation());
-    }
-
-    @Test
-    public void deliverResult_whenDeepLinkUrlDoesNotExistAndShouldNotNotifyCancellation_returnsNull() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-        when(activity.getIntent()).thenReturn(new Intent());
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request =
-                new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", false);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNull(result);
-        verify(persistentStore, never()).putActiveRequest(any(BrowserSwitchRequest.class), any(FragmentActivity.class));
-    }
-
-    @Test
-    public void deliverResult_whenRequestIsNull_doesNothing() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(null);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult result = sut.deliverResult(activity);
-
-        assertNull(result);
-        verify(persistentStore, never()).clearActiveRequest(activity);
-    }
-
-    @Test
-    public void captureResult_whenDeepLinkUrlExistsAndReturnUrlSchemeMatches_storesSuccessResultInPersistentStore() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-
-        Uri deepLinkUrl = Uri.parse("return-url-scheme://test");
-        Intent deepLinkIntent = new Intent().setData(deepLinkUrl);
-        when(activity.getIntent()).thenReturn(deepLinkIntent);
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request = new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", false);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        sut.captureResult(activity);
-
-        ArgumentCaptor<BrowserSwitchResult> captor =
-                ArgumentCaptor.forClass(BrowserSwitchResult.class);
-        verify(persistentStore).putActiveResult(captor.capture(), same(applicationContext));
-
-        BrowserSwitchResult result = captor.getValue();
-        assertNotNull(result);
-        assertEquals(123, result.getRequestCode());
-        assertSame(browserSwitchDestinationUrl, result.getRequestUrl());
-        assertEquals(BrowserSwitchStatus.SUCCESS, result.getStatus());
-        assertSame(requestMetadata, result.getRequestMetadata());
-        assertSame(deepLinkUrl, result.getDeepLinkUrl());
-    }
-
-    @Test
-    public void captureResult_whenRequestIsNull_doesNothing() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(null);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        sut.captureResult(activity);
-
-        verify(persistentStore, never()).putActiveResult(any(BrowserSwitchResult.class), any(Context.class));
-    }
-
-    @Test
-    public void captureResult_whenIntentIsNull_doesNothing() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-        when(activity.getIntent()).thenReturn(null);
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request = new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", false);
-        when(persistentStore.getActiveRequest(applicationContext)).thenReturn(request);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        sut.captureResult(activity);
-
-        verify(persistentStore, never()).putActiveResult(any(BrowserSwitchResult.class), any(Context.class));
-    }
-
-    @Test
-    public void deliverResultFromCache_forwardsCachedResultFromBrowserSwitchPersistentStorageAndRemovesAllItemsFromPersistentStorage() {
-        when(activity.getApplicationContext()).thenReturn(applicationContext);
-
-        JSONObject requestMetadata = new JSONObject();
-        BrowserSwitchRequest request = new BrowserSwitchRequest(123, browserSwitchDestinationUrl, requestMetadata, "return-url-scheme", false);
-        BrowserSwitchResult cachedResult = new BrowserSwitchResult(BrowserSwitchStatus.SUCCESS, request, Uri.parse("example://success/url"));
-        when(persistentStore.getActiveResult(same(applicationContext))).thenReturn(cachedResult);
-
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-        BrowserSwitchResult actualResult = sut.deliverResultFromCache(activity);
-
-        assertSame(cachedResult, actualResult);
-        verify(persistentStore).removeAll(applicationContext);
-    }
-
-    @Test
     public void parseResult_whenActiveRequestMatchesDeepLinkResultURLScheme_returnsBrowserSwitchSuccessResult() {
         BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
                 customTabsInternalClient);
@@ -444,14 +234,5 @@ public class BrowserSwitchClientUnitTest {
 
         BrowserSwitchResult browserSwitchResult = sut.parseResult(new BrowserSwitchPendingRequest.Started(request), null);
         assertNull(browserSwitchResult);
-    }
-
-    @Test
-    public void clearActiveRequests_forwardsInvocationToPersistantStore() {
-        BrowserSwitchClient sut = new BrowserSwitchClient(browserSwitchInspector,
-                customTabsInternalClient);
-
-        sut.clearActiveRequests(applicationContext);
-        verify(persistentStore).clearActiveRequest(applicationContext);
     }
 }
