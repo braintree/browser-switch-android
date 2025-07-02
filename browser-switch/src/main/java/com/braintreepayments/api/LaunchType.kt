4f80bbe614a73781b669795e9170f434337aab46
Timothy Chow <tichow@paypal.com>
2025-07-02 11:12:54 -0500

    Add changelog entry and kdoc for LaunchType

--- browser-switch/src/main/java/com/braintreepayments/api/LaunchType.kt
+++ browser-switch/src/main/java/com/braintreepayments/api/LaunchType.kt
@@ -1,5 +1,11 @@
 package com.braintreepayments.api
 
+/**
+ * Enum representing the type of launch for an activity.
+ *
+ * - [ACTIVITY_NEW_TASK]: sets the `Intent.FLAG_ACTIVITY_NEW_TASK` flag.
+ * - [ACTIVITY_CLEAR_TOP]: sets the `Intent.FLAG_ACTIVITY_CLEAR_TOP` flag.
+ */
 enum class LaunchType {
     ACTIVITY_NEW_TASK,
     ACTIVITY_CLEAR_TOP
