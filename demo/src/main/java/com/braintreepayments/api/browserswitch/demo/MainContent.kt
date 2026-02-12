7357f493bf73912c1d14207002b117f1c0a1db45
saperi <saperi@paypal.com>
2026-02-11 21:19:32 -0800

    linter

--- demo/src/main/java/com/braintreepayments/api/browserswitch/demo/MainContent.kt
+++ demo/src/main/java/com/braintreepayments/api/browserswitch/demo/MainContent.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.safeGesturesPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
